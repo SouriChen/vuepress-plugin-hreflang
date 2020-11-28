02391104e4f8dc843c1b44ef87e1e063d804fe96
陈泓君 <risas@tranhom.com>
2020-11-28 11:32:48 +0800

    如md有参数locales，则按照对应md的语言部分产生hreflang

--- hreflang.vue
+++ hreflang.vue
@@ -7,39 +7,38 @@
 
 export default {
   created() {
-    if (typeof this.$ssrContext !== "undefined") {
-      if (LANG.length != 0) {
-        this.computeURL()
+    if (typeof this.$ssrContext !== 'undefined') {
+        // md文件头部增加：locales: ['en','ja']
+      if (this.$page.frontmatter.locales) {
+        this.computeURL(this.$ssrContext.url)
+      } else {
+        if (LANG.length != 0) {
+          this.computeURL(this.$page.path)
+        }
       }
     }
   },
   methods: {
-    computeURL() {
-        let lang;
-        let langUrl = STRIPURL
-          ? this.$page.path.replace(/\.html$/, "")
-          : this.$page.path;
-        let url, path;
+    computeURL(params) {
+      let url, path, lang
+      let language = LANG
+      let langUrl = STRIPURL ? params.replace(/\.html$/, '') : params
+      let checkLang = langUrl.substring(1, langUrl.indexOf('/', 1))
+      if (language.indexOf(checkLang) !== -1) {
+        path = `${langUrl.substr(langUrl.indexOf('/', 1))}`
+      } else {
+        path = langUrl
+      }
 
-        let checkLang = langUrl.substring(1, langUrl.indexOf("/", 1));
-        if (LANG.indexOf(checkLang) !== -1) {
-          path = `${langUrl.substr(langUrl.indexOf("/", 1))}`;
+      language.map((hreflang) => {
+        if (hreflang !== 'en') {
+          url = `/${hreflang}${path}`
         } else {
-          path = langUrl;
+          url = path
         }
-
-        LANG.map((hreflang) => {
-          if (hreflang !== "en") {
-            url = `/${hreflang}${path}`;
-          } else {
-            url = path;
-          }
-
-          this.$ssrContext.userHeadTags += `<link rel='alternate' href='${
-            BASEURL + url
-          }' hreflang='${hreflang}'/>`;
-        });
+        this.$ssrContext.userHeadTags += `<link rel='alternate' href='${BASEURL + url}' hreflang='${hreflang}'/>`
+      })
     },
   },
-};
+}
 </script>
